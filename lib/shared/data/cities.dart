2255b376dba5cd7e69d175eff0367511236670fe
lucasmavila <lucas.mavila94@gmail.com>
2021-11-26 20:38:44 -0300

    change image size to 200

--- lib/shared/data/cities.dart
+++ lib/shared/data/cities.dart
@@ -2,7 +2,8 @@ const storesDataSet = [
   {
     "id": "1",
     "status": "active",
-    "logo": "https://images.unsplash.com/photo-1514933651103-005eec06c04b",
+    "logo":
+        "https://unsplash.com/photos/GXXYkSwndP4/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8cmVzdGF1cmFudHx8MHx8fHwxNjM3OTE2ODY1&force=true&w=200",
     "name": "Hamburgueria do José",
     "address": {
       "cep": "03043020",
@@ -21,8 +22,8 @@ const storesDataSet = [
         "mainPhoto": 0,
         "price": 16.5,
         "images": [
-          "https://images.unsplash.com/photo-1595531172949-30922c28a240",
-          "https://images.unsplash.com/photo-1609167830220-7164aa360951"
+          "https://images.unsplash.com/photo-1595531172949-30922c28a240?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200",
+          "https://images.unsplash.com/photo-1623944637197-814c1020ddd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200"
         ],
         "additionalItems": [
           {"name": "alface", "price": 1.00},
@@ -37,7 +38,9 @@ const storesDataSet = [
         "description": "pão, burguer veg 150g, cheddar, tomate, cebola e molho",
         "mainPhoto": 0,
         "price": 18.5,
-        "images": ["https://images.unsplash.com/photo-1551615593-ef5fe247e8f7"],
+        "images": [
+          "https://images.unsplash.com/photo-1551615593-ef5fe247e8f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200"
+        ],
         "additionalItems": [
           {"name": "alface", "price": 1.00},
           {"name": "cheddar", "price": 3.00},
@@ -52,8 +55,8 @@ const storesDataSet = [
         "mainPhoto": 0,
         "price": 12.5,
         "images": [
-          "https://images.unsplash.com/photo-1573080496219-bb080dd4f877",
-          "https://images.unsplash.com/photo-1580559489333-755f7e3c8949"
+          "https://images.unsplash.com/photo-1573080496219-bb080dd4f877?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200",
+          "https://images.unsplash.com/photo-1580559489333-755f7e3c8949?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200"
         ],
         "additionalItems": null
       }
